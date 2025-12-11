c1877b54df1315c6da2d4e4de20015e43f1b5490
M-Jorda <j.jorda@gmail.com>
2025-12-11 06:19:43 +0100

    ex00 done

---
+++ CPP01/ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 00:00:00 by                   #+#    #+#             */
+/*   Updated: 2025/12/11 06:10:02 by jjorda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			char c = argv[i][j];
+			if (c >= 'a' && c <= 'z')
+				c -= 32;
+			std::cout << c;
+		}
+	}
+	
+	std::cout << std::endl;
+	return (0);
+}
