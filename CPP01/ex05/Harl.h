57bb32a008aa85b3e512ad7095b57757cb5a1de7
M-Jorda <j.jorda@gmail.com>
2025-12-20 17:43:14 +0100

    Harl done

---
+++ CPP01/ex05/Harl.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/20 17:01:35 by jjorda            #+#    #+#             */
+/*   Updated: 2025/12/20 17:20:15 by jjorda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_H
+# define HARL_H
+
+# include <string>
+# include <iostream>
+
+# define DEBUG		"[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+# define INFO		"[INFO] I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+# define WARNING	"[WARNING] I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month."
+# define ERROR		"[ERROR] This is unacceptable! I want to speak to the manager now."
+
+#endif
