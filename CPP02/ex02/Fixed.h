071dae932d4dd39e4b4b8d498ad9fb2f2fb7637e
M-Jorda <j.jorda@gmail.com>
2025-12-21 15:54:31 +0100

    Fixed done

---
+++ CPP02/ex02/Fixed.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/21 11:35:16 by jjorda            #+#    #+#             */
+/*   Updated: 2025/12/21 12:36:15 by jjorda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+# include "iostream"
+# include "string"
+# include "cmath"
+
+# define DEFAULT	"Default constructor called"
+# define INT		"Int constructor called"
+# define FLOAT		"Float constructor called"
+# define COPY		"Copy constructor called"
+# define ASSIGN		"Copy assignment operator called"
+# define GETRAW		"getRawBits member function called"
+# define DESTRUCTOR	"Destructor called"
+
+#endif
