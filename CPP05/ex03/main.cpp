2918d49a5c307b20f381730176e743f497aa3d11
M-Jorda <jorda.j.fr@gmail.com>
2026-06-02 18:31:22 +0200

    ex03 done

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 13:07:05 by jjorda            #+#    #+#             */
-/*   Updated: 2026/06/02 18:07:11 by jjorda           ###   ########.fr       */
+/*   Updated: 2026/06/02 18:31:13 by jjorda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,37 @@
 #include "PresidentialPardonForm.hpp"
 #include "Intern.hpp"
 
+#include <ctime>
+
+void	FormExec(AForm *f, const Bureaucrat &boss, const Bureaucrat &badBoy)
+{
+	if (f)
+	{
+		f->beSigned(boss);
+		boss.executeForm(*f);
+		badBoy.executeForm(*f);
+	}
+}
+
 int main(void)
 {
-    Intern someRandomIntern;
-    AForm* f1 = someRandomIntern.makeForm("robotomy request", "Bender");
-    AForm* f2 = someRandomIntern.makeForm("shrubbery creation", "garden");
-    AForm* f3 = someRandomIntern.makeForm("presidential pardon", "Arthur");
-    AForm* f4 = someRandomIntern.makeForm("invalid form", "nobody");
+	std::srand(std::time(NULL));
+
+	Bureaucrat	boss("Boss", 1);
+	Bureaucrat	badBoy("The Bad Boy", 150);
+	Intern someRandomIntern;
+
+	AForm* f1 = someRandomIntern.makeForm("robotomy request", "Bender");
+	FormExec(f1, boss, badBoy);
+	
+	AForm* f2 = someRandomIntern.makeForm("shrubbery creation", "garden");
+	FormExec(f2, boss, badBoy);
+	
+	AForm* f3 = someRandomIntern.makeForm("presidential pardon", "Arthur");
+	FormExec(f3, boss, badBoy);
+	
+	AForm* f4 = someRandomIntern.makeForm("invalid form", "nobody");
+	FormExec(f4, boss, badBoy);
 
     delete f1;
     delete f2;
