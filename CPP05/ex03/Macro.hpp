7247834d21000ca13c2a3d2242284c38cfe9d91a
M-Jorda <jorda.j.fr@gmail.com>
2026-06-26 12:59:11 +0200

    CPP05 cleaned

--- CPP05/ex03/Macro.hpp
+++ CPP05/ex03/Macro.hpp
@@ -6,31 +6,31 @@
 /*   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 17:46:38 by jjorda            #+#    #+#             */
-/*   Updated: 2026/06/02 18:29:46 by jjorda           ###   ########.fr       */
+/*   Updated: 2026/06/16 15:42:16 by jjorda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MACRO_HPP
 # define MACRO_HPP
 
-# define MSG_CONS			" constructor called"
-# define MSG_DES				" destructor called"
-# define MSG_DF_CONS			" default" MSG_CONS
-# define MSG_CP_CONS			" copy" MSG_CONS
+# define MSG_CONS		" constructor called"
+# define MSG_DES		" destructor called"
+# define MSG_DF_CONS	" default" MSG_CONS
+# define MSG_CP_CONS	" copy" MSG_CONS
 
-# define DRILL				"*drilling noises*"
-# define SUCCES_RBTMZD		" has successfully been robotomized"
-# define FAILED_RBTMZD		" has failed to be robotomized"
+# define DRILL			"*drilling noises*"
+# define SUCCES_RBTMZD	" has successfully been robotomized"
+# define FAILED_RBTMZD	" has failed to be robotomized"
 
-# define PARDON				" has been pardoned by Zaphod Beeblebrox."
+# define PARDON			" has been pardoned by Zaphod Beeblebrox."
 
-# define SHRUBNAME			"_shrubbery"
-# define SHRUBTREES			"   *        *\n  ***      ***\n *****    *****\n\
+# define SHRUBNAME		"_shrubbery"
+# define SHRUBTREES		"   *        *\n  ***      ***\n *****    *****\n\
 *******  *******\n   |        |"
 
-# define GRADETOOLOW		"Grade is too low"
-# define GRADETOOHIGH		"Grade is too high"
-# define FORMNOTSIGNED		"This form is not signed"
-# define ERRCREATE			"Cannot created the output file"
+# define GRADETOOLOW	"Grade is too low"
+# define GRADETOOHIGH	"Grade is too high"
+# define FORMNOTSIGNED	"This form is not signed"
+# define ERRCREATE		"Cannot created the output file"
 
 #endif
